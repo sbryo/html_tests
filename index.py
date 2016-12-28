14db88af7db54016f5b458312ed17130131db7e0
ShakedBraimok <shaked.braimok@startapp.com>
2016-12-28 12:54:33 +0200

    added

--- index.py
+++ index.py
@@ -43,13 +43,46 @@ def accept(ID):
             		return flask.redirect('/confirmed')
 	return flask.redirect('/confirmed')
 
+@app.route("/reset_password/<ID>", methods=['GET','POST'])
+def reset_password(ID):
+	client = MongoClient('ds019254.mlab.com', 19254)
+    	client.users.authenticate('shakedinero','a57821688')
+    	db = client.users
+    	collection = db.users
+    	cursor = db.users.find()
+	for doc in cursor:
+        	if str(doc['_id']) == str(ID):
+                mail = str(doc['email'])
+	return flask.render_template('reset_form.html',email=email)
+
+@app.route("/reset/<EMAIL>",methods=['GET','POST'])
+def reset(EMAIL):
+    if ("password1" in flask.request.form) and ("password2" in flask.request.form):
+        if (flask.request.form['password1']) == flask.request.form['password2']:
+            password = flask.request.form['password1']
+            for doc in cursor:
+                if doc['email] == EMAIL:
+                    mongo_id=doc['_id']
+                    post = collection.find_one({"_id":mongo_id})
+                    post['password'] = password
+                    collection.update({'_id':mongo_id}, {"$set": post}, upsert=False)
+                    return flask.redirect('/reset_done')
+        else:
+            #return template "passwords not equal ! "
+            return
+    else:
+        #return template "please fill all the fields"
+        return
+    return flask.render_template('/reset_done')
+
+
 @app.route("/confirmed")
 def confirmed():
 	return flask.render_template("confirm_signup.html")
 
 @app.route("/reset_done")
-def reset():
-	return flask.render_template("reset_pass.html")
+def reset_done():
+	return flask.render_template("reseted_pass.html")
 
 
 
